e2973a96d4d9e2973912a75802cde8bdb2d05595
CompressedDuck <ignacyrozen@duck.com>
2025-10-06 22:18:41 +0200

    aaaaaaaaaaaaa

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,27 +1,27 @@
 import scrapeData from "./server-functions/scrapedata";
-import "cally"
+import Calendar from "./web-copmonents/calendar";
 
 export default async function Home() {
   const data = await scrapeData();
-  const openEvents = []
+  
+  function filterEvent(){
+
+  };
+
   return (
     <main>
       <div className="flex flex-row m-4">
         <div className="box p-4 border-2 border-solid border-gray-200 rounded-xl">
-          <calendar-range>
-            <calendar-month></calendar-month>
-          </calendar-range>
+          <Calendar />
         </div>
       </div>
       <div className="events">
       {data.events.map((event) => (
-        <div tabIndex={0} className="collapse bg-base-100 border-base-300 border">
+        <div key={event.id} tabIndex={0} className="collapse bg-base-100 border-base-300 border">
           <div className="collapse-title font-semibold">
-            <span className="text-primary-content"></span>
+            <span className="text-primary-content">{event.title}</span>
           </div>
-          <div className="collapse-content text-sm">
-            {event.description}
-          </div>
+          <div className="collapse-content text-sm">{event.description}</div>
         </div>
       ))}
       </div>
---
+++ src/app/web-copmonents/calendar.jsx
@@ -0,0 +1,10 @@
+"use client";
+import "cally";
+
+export default function Calendar() {
+  return (
+      <calendar-range>
+        <calendar-month></calendar-month>
+      </calendar-range>
+  );
+}
