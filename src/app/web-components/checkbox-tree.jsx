9f97a2a1e763c068b189eb0a97b29853c92c6ae9
duck <ignacyrozen@duck.com>
2025-10-08 19:32:13 +0200

    nothing works probably

--- src/app/web-components/checkbox-tree.jsx
+++ src/app/web-components/checkbox-tree.jsx
@@ -1,27 +1,30 @@
-export default function CheckboxTree({ iterable }) {
+"use client";
 
+export default function CheckboxTree({ iterable }) {
     return (
-        <ul>
-            {Object.keys(iterable).map((key) => (
-                <li key={key}>
-                    <div tabIndex={0} className="collapse">
-                        <div className="collapse-title font-semibold">
-                            <label className="label">
-                                <input type="checkbox" defaultChecked className="checkbox" />
-                                {key}
-                            </label>
-                        </div>
-                        <div className="collapse-content text-sm">
-                            {iterable[key].map((item, index) => (
-                                <label key={index} className="label">
+        <>
+            <ul className="bg-base-200 border border-base-300 rounded-box">
+                {Object.keys(iterable).map((key) => (
+                    <li key={key}>
+                        <div tabIndex={0} className="collapse">
+                            <div className="collapse-title font-semibold">
+                                <label className="label">
                                     <input type="checkbox" defaultChecked className="checkbox" />
-                                    {item}
+                                    {key}
                                 </label>
-                            ))}
+                            </div>
+                            <div className="collapse-content grid-collumns-2 grid text-sm">
+                                {iterable[key].map((item, index) => (
+                                    <label key={index} className="label mt-2">
+                                        <input value={item} type="checkbox" defaultChecked className="checkbox" />
+                                        {item}
+                                    </label>
+                                ))}
+                            </div>
                         </div>
-                    </div>
-                </li>
-            ))}
-        </ul>
+                    </li>
+                ))}
+            </ul>
+        </>
     );
 }
--- src/app/web-components/event.jsx
+++ src/app/web-components/event.jsx
@@ -1,15 +1,17 @@
 
 export default function Event({ event,filterData }) {
-    let showEvent = false;
-    if (new Date(filterData.endDate) > new Date(event.date) > new Date(filterData.startDate)) {
-        showEvent = true;
+    let showEvent = true;
+    if (!(new Date(filterData.endDate) > new Date(event.date) > new Date(filterData.startDate))) {
+        showEvent = false;
     }
-    else if (filterData.startDate === null && filterData.endDate === null) {
+    else if (filterData.startDate != null && filterData.endDate != null) {
+        showEvent = false;
+    } else if (!filterData.activelocations.includes(event.location[0]) && filterData.activelocations.length != 0) { 
         showEvent = true;
-    } else if (filterData.activelocations.includes(event.location[0]) || filterData.activelocations.length === 0) { 
-        showEvent = true;
+    } else if (filterData.searchTerm && filterData.searchTerm != ""&& (!event.description.tolowerCase().includes(filterData.searchTerm.toLowerCase()) || !event.title.toLowerCase().includes(filterData.searchTerm.toLowerCase()))) {
+        showEvent = false;
     }
-
+    showEvent = true;
     if (!showEvent) return null;
     return (
     <div key={event.id} tabIndex={0} className="w-auto collapse border-base-300 border-1 p-2 bg-base-200 m-4 rounded-box">
